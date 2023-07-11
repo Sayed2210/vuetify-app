a7d7a3ce39046f0f3c7e9812c7aea67470e3fe5a
Elsayed Hassan <eh600039@gmail.com>
2023-07-11 12:19:30 +0300

    22%

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,7 +18,7 @@
     ></v-btn>
     <v-container>
       <v-row>
-        <v-col :for="num in 4" :key="num">
+        <v-col v-for="num in numbers" :key="num">
           <v-card class="mx-auto" max-width="344">
             <v-img
               src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
@@ -67,12 +67,165 @@
       >
       </v-expansion-panel>
     </v-expansion-panels>
+    <v-btn color="primary" id="my_menu"> Dropdown </v-btn>
+    <v-menu activator="#my_menu" location="bottom">
+      <v-list :items="items"></v-list>
+    </v-menu>
+    <v-layout>
+      <v-navigation-drawer permanent absolute>
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://cdn.vuetifyjs.com/images/john.png"
+            title="John Leider"
+            subtitle="john@google.com"
+          >
+            <template v-slot:append>
+              <v-btn size="small" variant="text" icon="mdi-menu-down"></v-btn>
+            </template>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list :lines="false" density="compact" nav>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            :value="item"
+            color="primary"
+          >
+            <template v-slot:prepend>
+              <v-icon :icon="item.icon"></v-icon>
+            </template>
+
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 354px"></v-main>
+    </v-layout>
+    <v-toolbar
+      dark
+      prominent
+      image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-toolbar extended extension-height="100" class="ma-10">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-divider></v-divider>
+    <v-btn>
+      Start
+      <v-tooltip class="ma-10" activator="parent" location="right"
+        >Tooltip</v-tooltip
+      >
+    </v-btn>
+    <v-layout>
+      <v-app-bar color="red" density="compact">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>Photos</v-app-bar-title>
+
+        <template v-slot:append>
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+      </v-app-bar>
+
+      <v-main>
+        <v-container fluid>
+          <v-row dense>
+            <v-col v-for="n in 8" :key="n" cols="3">
+              <v-sheet color="grey-lighten-2" height="96"></v-sheet>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+      <v-bottom-navigation>
+        <v-btn value="recent">
+          <v-icon>mdi-history</v-icon>
+
+          Recent
+        </v-btn>
+
+        <v-btn value="favorites">
+          <v-icon>mdi-heart</v-icon>
+
+          Favorites
+        </v-btn>
+
+        <v-btn value="nearby">
+          <v-icon>mdi-map-marker</v-icon>
+
+          Nearby
+        </v-btn>
+      </v-bottom-navigation>
+      <v-breadcrumbs :items="links">
+        <template v-slot:prepend>
+          <v-icon size="small" icon="$vuetify"></v-icon>
+        </template>
+      </v-breadcrumbs>
+    </v-layout>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+const numbers = ref([1, 2, 3]);
 const loading = ref(false);
+const items = ref([
+  { text: "My Files", icon: "mdi-folder" },
+  { text: "Shared with me", icon: "mdi-account-multiple" },
+  { text: "Starred", icon: "mdi-star" },
+  { text: "Recent", icon: "mdi-history" },
+  { text: "Offline", icon: "mdi-check-circle" },
+  { text: "Uploads", icon: "mdi-upload" },
+  { text: "Backups", icon: "mdi-cloud-upload" },
+]);
+const links = ref([
+  {
+    title: "Dashboard",
+    disabled: false,
+    href: "breadcrumbs_dashboard",
+  },
+  {
+    title: "Link 1",
+    disabled: false,
+    href: "breadcrumbs_link_1",
+  },
+  {
+    title: "Link 2",
+    disabled: true,
+    href: "breadcrumbs_link_2",
+  },
+]);
 const clickme = () => {
   loading.value = true;
   setTimeout(() => {
